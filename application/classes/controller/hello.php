29a2d143a7070d79c68de5f693eaf8d17f036eae
Ralph Bean <ralph.bean@gmail.com>
2010-07-25 14:55:01 -0400

    Learning views.

--- application/classes/controller/hello.php
+++ application/classes/controller/hello.php
@@ -12,16 +12,46 @@ Class Controller_Hello extends Controller
 
 	function action_index()
 	{
-		$this->request->response = 'hello, worldz but better...';
-	}
-	
-	function action_test()
-	{
-		echo 'test, lolz';
+		$this->request->redirect('index.php/hello/no_diggity/1');
 	}
 
-	function action_dynamic($say)
+	function action_no_diggity($the_way_you_work_it)
 	{
-		$this->request->response = 'You said:  '.$say;
+		// A constant for this demonstration
+		$msg = 'Try passing in values between 1 and 4';
+	
+		// Some sanity checking	
+		if ( $the_way_you_work_it < 1 or $the_way_you_work_it > 4 )
+		{
+			$msg = 'Between 1 and 4!  :(';
+			$the_way_you_work_it = 1;
+		}
+
+		// I like the way you work it.  No diggity.
+		if ( $the_way_you_work_it == 1 )
+		{
+			$view = View::factory('pages/hello');
+			$view->content = $msg;
+			$this->request->response = $view->render();
+		}
+		elseif ( $the_way_you_work_it == 2 )
+		{
+			$data['content'] = $msg;
+			$view = View::factory('pages/hello', $data);
+			$this->request->response = $view->render();
+		}
+		elseif ( $the_way_you_work_it == 3 )
+		{
+			$view = View::factory('pages/hello')->set('content', $msg);
+			$this->request->response = $view->render();
+		}
+		elseif ( $the_way_you_work_it == 4 )
+		{
+			$content = 'fooblong';
+			$view = View::factory('pages/hello')->bind('content', $content);
+			// Well no shit...
+			$content = $msg;
+			$this->request->response = $view->render();
+		}
 	}
 }
---
+++ application/views/pages/hello.php
@@ -0,0 +1,9 @@
+<html>
+	<head>
+		<title>Hello!</title>
+	</head>
+	<body>
+		<h1>This is my first view</h1>
+		<p>Content:  '<?php echo $content; ?>' </p>
+	</body>
+</html>
