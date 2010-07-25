bcd1008332f60d52d948d3552c24401e08a8bd41
HARRY GROOVER <harrygroover@ubuntu.localdomain>
2010-07-24 23:50:34 -0400

    derrrrrrrrrrr

--- application/logs/2010/07/23.php
+++
@@ -1,7 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.'); ?>
-
-2010-07-23 15:39:16 --- ERROR: ReflectionException [ 0 ]: Method action_test does not exist ~ SYSPATH/classes/kohana/request.php [ 1046 ]
-2010-07-23 15:51:18 --- ERROR: ErrorException [ 4096 ]: Argument 1 passed to Kohana_Controller::__construct() must be an instance of Kohana_Request, none given, called in /var/www/kohana/testapp/application/classes/controller/hello.php on line 8 and defined ~ SYSPATH/classes/kohana/controller.php [ 37 ]
-2010-07-23 16:00:19 --- ERROR: Kohana_Request_Exception [ 0 ]: Unable to find a route to match the URI: index.php ~ SYSPATH/classes/kohana/request.php [ 638 ]
-2010-07-23 16:00:42 --- ERROR: Kohana_Request_Exception [ 0 ]: Unable to find a route to match the URI: index.php ~ SYSPATH/classes/kohana/request.php [ 638 ]
-2010-07-23 16:00:44 --- ERROR: Kohana_Request_Exception [ 0 ]: Unable to find a route to match the URI: index.php ~ SYSPATH/classes/kohana/request.php [ 638 ]
--- application/logs/2010/07/24.php
+++
@@ -1,5 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.'); ?>
-
-2010-07-24 20:26:11 --- ERROR: Kohana_Request_Exception [ 0 ]: Unable to find a route to match the URI: index.php ~ SYSPATH/classes/kohana/request.php [ 638 ]
-2010-07-24 20:40:00 --- ERROR: ReflectionException [ 0 ]: Method action_test does not exist ~ SYSPATH/classes/kohana/request.php [ 1046 ]
-2010-07-24 23:33:00 --- ERROR: ErrorException [ 8 ]: Undefined variable: der ~ APPPATH/classes/controller/hello.php [ 20 ]
