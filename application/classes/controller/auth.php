dbc9653c4260f157ebef8c06829067d9b10a72f9
Ralph Bean <ralph.bean@gmail.com>
2010-07-25 23:30:00 -0400

    Progress on auth stuff.

---
+++ application/classes/controller/auth.php
@@ -0,0 +1,69 @@
+<?php
+defined('SYSPATH') OR die('No Direct Script Access');
+ 
+Class Controller_Auth extends Controller
+{
+	// TODO -- this should be passed into the constructor or something.
+	// TODO -- OR, use a default root controller?
+	private $default_destination = 'index.php/hello';
+
+	function __construct(Kohana_Request $request)
+	{
+		parent::__construct($request);
+		$this->profiler = new Profiler;
+	}
+
+	function action_index()
+	{
+		$this->request->redirect('index.php/auth/login');
+	}
+
+	function action_login()
+	{
+		if ( isset( $_GET['destination'] ) )
+		{
+			$ref = $_GET['destination'];
+		}
+		elseif ( Request::$referrer != '' )
+		{
+			$ref = Request::$referrer;
+		}
+		else
+		{
+			$ref = $this->default_destination;
+		}
+		print "chose ref: $ref<br>";
+		$view = View::factory('pages/auth/login');
+		$view->destination = $ref;
+		$this->request->response = $view->render();
+	}
+	
+	function action_logout()
+	{
+		Auth::instance()->logout();
+		// TODO -- flash message about succes or whatever
+		$this->request->redirect($this->default_destination);
+	}
+
+	function action_do_login()
+	{
+		$destination = $_POST['destination'];
+		$username = $_POST['username'];
+		$password = $_POST['password'];
+		$auth = Auth::instance();
+		$success = $auth->login($username, $password);
+		if ( $success == True )
+		{
+			// TODO -- flash about success?
+			// flash here  git://github.com/daveWid/message.git
+			$this->request->redirect($destination);
+		}
+		else
+		{
+			// TODO -- flash about failure?
+			// flash here  git://github.com/daveWid/message.git
+			$this->request->redirect("index.php/auth/login?destination=$destination");
+		}
+	}
+
+}
