e05bd81d4fc43883398006b6ee763afd64ea9b76
Ralph Bean <ralph.bean@gmail.com>
2010-07-25 21:03:03 -0400

    login/logout base functionality test.

--- application/classes/controller/hello.php
+++ application/classes/controller/hello.php
@@ -10,10 +10,45 @@ Class Controller_Hello extends Controller
 		$this->profiler = new Profiler;
 	}
 
+
 	function action_index()
 	{
 		$this->request->redirect('index.php/hello/no_diggity/1');
 	}
+
+	/*
+	 * Hey Balz... so I'm just screwing around with testing stuff here.
+     * This is by no means a long-term shtick.
+     *
+     * but hey.. I got basic file-based auth working
+     * ...gonna hang onto it for now and do sql later in the game.
+     * this is hyper easy to manage during development.
+     */
+	// This is interested if you go to 
+	// http://localhost/kohana/ptododactyl/index.php/hello/login/bananas
+	function action_login($password)
+	{
+		$auth = Auth::instance();
+		$username = 'admin';
+		// The real password is 'bananas'
+
+		echo "<p>logged in before?  ";
+		echo $auth->logged_in() ? 'Yes!' : 'No.';
+		echo "</p>";
+		
+		$auth->logout();
+		
+		echo "<p>...and now?  ";
+		echo $auth->logged_in() ? 'Yes!' : 'No.';
+		echo "</p>";
+		
+		$auth->login($username, $password);
+		
+		echo "<p>...and now?  ";
+		echo $auth->logged_in() ? 'Yes!' : 'No.';
+		echo "</p>";
+	}
+
 
 	function action_no_diggity($the_way_you_work_it)
 	{
