0b50dd4f5ff71d8fb3600f88c6713cae552d99cb
Ralph Bean <ralph.bean@gmail.com>
2010-07-25 21:02:38 -0400

    added a test user

--- application/config/auth.php
+++ application/config/auth.php
@@ -10,7 +10,9 @@ return array(
 
 	// Username/password combinations for the Auth File driver
 	'users' => array(
-		// 'admin' => 'b3154acf3a344170077d11bdb5fff31532f679a1919e716a02',
+		'admin' => '6325bb7527fd6b3db8432f88d488d361ad27246e1d452a55d8',
+		// ...which stands for 'bananas' in crypto land.  I got this by using
+		// Auth::instance()->hash_password('bananas'); 
 	),
 
 );
